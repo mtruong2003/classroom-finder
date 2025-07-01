1c43151736ed220cd75ffb7fdd7b8e6fed74efc3
Minh Truong <minhtruong@mac.lan>
2025-06-30 21:40:58 -0400

    add QST data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import json 
 
 st.title("Classroom Finder")
 st.write(
-    "@classroomfinder"
+    "QST"
 )
+
+with open("QST.json") as file:
+        data = json.load(file)
+        st.write(data)
